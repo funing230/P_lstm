03f6fa5157e70ae70aa4f6d639a7c4964125767f
funing <funing230@gmail.com>
2021-09-20 14:13:01 +0900

    09201412

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ from keras.models import Model
 import keras
 from sklearn.preprocessing import LabelEncoder
 
-import sys
-sys.setrecursionlimit(10000)
+
+
+
 
 #read bugreport for XLSX
 projectname = 'Eclipse_Platform_UI_bugreport'
@@ -30,26 +31,34 @@ Eclipse_Platform_UI = pd.read_excel('dataset/' + projectname + '.xlsx', engine='
 Eclipse_Platform_UI.rename(columns = {'Unnamed: 10' : 'result'}, inplace = True)
 
 #read weight table
-weight=pd.read_excel('dataset/' +'writer.xlsx', engine='openpyxl',header=None,nrows=5)
-#processing weight
-weight= weight.fillna(0)
+weight=pd.read_excel('writer.xlsx', engine='openpyxl',header=None,nrows=5)
+weight= weight.iloc[:,1:].fillna(0)
+maxmin = preprocessing.MinMaxScaler()
+# print(weight.dtypes)
 
-p_weight=pd.get_dummies(weight,drop_first=True)
+def is_simple_numpy_number(dtype):
+    if np.issubdtype(dtype, np.integer):
+        return True
+    if np.issubdtype(dtype, np.floating):
+        return True
+    return False
 
-lastweight=preprocessing.scale(p_weight.iloc[:,1:])
-lastweight= pd.DataFrame(lastweight);
-lastweight.insert(0,'bug_id',1, allow_duplicates=False)
-lastweight['bug_id']=weight[0]
 
-lastweight= lastweight.drop('bug_id', 1)
+for col in range(weight.shape[1]):
+    if is_simple_numpy_number(weight.iloc[:, col].dtype):
+        scale_param = maxmin.fit(weight.iloc[:, col].values.reshape(-1, 1))
+        weight.iloc[:, col]=maxmin.fit_transform(weight.iloc[:, col].values.reshape(-1, 1),scale_param)
 
+lastweight=pd.get_dummies(weight)
+print(lastweight.info())
 print(lastweight.head(2))
+
 #read Serverity for XLSX
 serverity = pd.read_excel('dataset/' + 'serverity' + '.xlsx', engine='openpyxl',nrows=5)
 serverity.rename(columns = {'Unnamed: 10' : 'result'}, inplace = True)
 
 #concat serverity with bugreport
-#Eclipse_Platform_UI=pd.concat([Eclipse_Platform_UI, lastweight,serverity], axis=1,keys='bug_id')
+
 Eclipse_Platform_UI=pd.concat([Eclipse_Platform_UI, serverity], axis=1,keys='bug_id')
 print(Eclipse_Platform_UI.head(10))
 
@@ -85,8 +94,8 @@ messages.reset_index(inplace = True)
 #messages['title']
 
 #Download the nltk toolkit
-#nltk.download('stopwords')
-#nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('punkt')
 
 
 ### Dataset Preprocessing
@@ -148,8 +157,8 @@ def tokenizer(sentences, word_index):
 
 
 # id summary description serverity
-
-summary_description_word2vec_idex=tokenizer(df_id_br_s.iloc[:,1]+df_id_br_s.iloc[:,2], word_index)
+#merge=df_id_br_s.iloc[:,1]+df_id_br_s.iloc[:,2]
+summary_description_word2vec_idex=tokenizer(sentences, word_index)
 print(summary_description_word2vec_idex.shape)
 print(lastweight.shape)
 
@@ -178,7 +187,7 @@ print(model.summary())
 main_input = summary_description_word2vec_idex
 aux_input = lastweight
 
-# 通过字典的形式将数据fit到模型
+# fit
 model.fit(
     {"bugreport": summary_description_word2vec_idex, "weight": lastweight},
     {"output": y},
@@ -186,91 +195,7 @@ model.fit(
     batch_size=32,
 )
 
-#keras.utils.plot_model(model, "multi_input_and_output_model.png", show_shapes=True)
-
-# #main_input = summary_description_word2vec_idex
-#
-# main_input = Input((500,), dtype='float64', name='main_input')
-#
-# x = Embedding(output_dim=embeddings_matrix.shape[0], input_dim=embeddings_matrix.shape[1], input_length=300)(main_input)
-#
-# lstm_out = LSTM(64)(x)
-#
-# aux_input = Input((lastweight.shape[1],), name='aux_input')
-#
-# x = keras.layers.concatenate([lstm_out, aux_input])
-# x = Dense(64, activation='relu')(x)
-# x = Dense(64, activation='relu')(x)
-# x = Dense(64, activation='relu')(x)
-# #x = tf.keras.layers.Flatten(x)
-# #model.add(tf.keras.layers.Flatten())
-# main_output = Dense(1, activation='softmax', name='main_output')(x)
-#
-# model = Model(inputs=[main_input, aux_input], outputs=main_output)
-# model.compile(optimizer='rmsprop',
-# loss={'main_output': keras.losses.CategoricalCrossentropy(from_logits=True)},
-# loss_weights={'main_output': 0.2})
-# print(model.summary())
-
-main_input = summary_description_word2vec_idex
-aux_input = lastweight
-
-
-# # Dummy target data
-# priority_targets = np.random.random(size=(1280, 1))
-# # dept_targets = np.random.randint(2, size=(1280, num_departments))
-
-
 #
 
 
-
-
-
-
-
-#
-# #X = tokenizer(sentences, word_index) #texts is numpy, input into the model calculation.
-#
-# #embeddings_matrix.shape
-#
-# model = keras.Sequential([
-#       keras.layers.Embedding(input_dim=embeddings_matrix.shape[0],
-#                              output_dim=embeddings_matrix.shape[1],
-#                              weights=[embeddings_matrix],
-#                              input_length=20),
-#       keras.layers.LSTM(200),
-#       keras.layers.Dropout(0.3),
-#       keras.layers.Dense(1, activation='sigmoid')
-#    ])
-# model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
-#
-# model.summary()
-#
-#
-# X_final=X   #np.array(X)
-# y_final=np.array(y)
-#
-# X_final.shape,y_final.shape
-#
-# from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X_final, y_final, test_size=0.30, random_state=42)
-#
-# ### Finally Training
-# model.fit(X_train,y_train,validation_data=(X_test,y_test),epochs=20,batch_size=64)
-#
-#
-# y_pred=(model.predict(X_test)>=0.5).astype("int")   # use sigmoid value
-#
-#
-# from sklearn.metrics import confusion_matrix
-#
-# confusion_matrix(y_test,y_pred)
-#
-# from sklearn.metrics import accuracy_score
-# print(accuracy_score(y_test,y_pred))
-#
-#
-#
-#
 
